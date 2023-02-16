742c030df780c0909f00371bba242b445d7f3f86
rocio_martin98 <rocio_martin98@hotmail.com>
2023-02-16 14:02:19 -0300

    our job developer, not finished

--- src/components/OurJob.jsx
+++ src/components/OurJob.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function OurJob() {
+  const web = require("../assets/web.png");
+  const movil = require("../assets/movil.png");
+
   return (
-    <div>
-      
+    <div style={{ alignItems: "center", marginLeft: "30%" }}>
       <div className="whoAreBox">
-      <p className="hoverTextSec">
-      Conoce nuestros proyectos destacados.
-      </p>
-      
+        <p className="hoverTextSec">Conoce nuestros proyectos destacados.</p>
+      </div>
+      <div className="divImagesOurJob">
+        <button className="buttonOurJob">
+          <img height={150} width={150} src={web}></img>
+        </button>
+        <button className="buttonOurJob">
+          <img height={150} width={150} src={movil}></img>
+        </button>
+      </div>
     </div>
-
-    </div>
-
-
-
-  )
+  );
 }
